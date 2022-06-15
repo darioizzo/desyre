9a6867b8ed29e829ba64fb1f1be57b46a925e6cf
Dario Izzo <dario.izzo@gmail.com>
2022-06-15 19:20:15 +0200

    a first

---
+++ main.cpp
@@ -0,0 +1,162 @@
+#include <assert.h> /* assert */
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+
+#include <fmt/ranges.h>
+
+// Binary kernels
+double add(double a, double b)
+{
+    return a + b;
+}
+
+double sub(double a, double b)
+{
+    return a - b;
+}
+
+double mul(double a, double b)
+{
+    return a * b;
+}
+
+double div(double a, double b)
+{
+    return a / b;
+}
+
+struct expression {
+    expression(unsigned nvar, unsigned ncon, std::vector<unsigned> kernels,
+               decltype(std::random_device{}()) seed = std::random_device{}())
+        : m_nvar(nvar), m_ncon(ncon), m_kernels(kernels), m_rng(seed)
+    {
+        m_nker = kernels.size();
+    };
+
+    std::vector<unsigned> random_genotype(unsigned length)
+    {
+        std::vector<unsigned> retval(3 * length);
+        unsigned nus = 0u;
+        std::uniform_int_distribution<int> uni_ker(0, m_nker - 1);
+        for (auto i = 0u; i < length; ++i) {
+            // Lets pick a random kernel
+            unsigned funidx = uni_ker(m_rng);
+            // Lets pick a random u0
+            std::uniform_int_distribution<int> uni_us(0, m_nvar + nus - 1);
+            unsigned u0idx = uni_us(m_rng);
+            unsigned u1idx = u0idx;
+            // ... and a random u1 different from u0 if the kernel is subtraction (idx = 1)
+            if (funidx != 1) {
+                u1idx = uni_us(m_rng);
+            } else {
+                while (u1idx == u0idx) {
+                    u1idx = uni_us(m_rng);
+                    if (i == 0u && (m_nvar + m_ncon) == 1u) break;
+                }
+            }
+            retval[3 * i] = funidx;
+            retval[3 * i + 1] = u0idx;
+            retval[3 * i + 2] = u1idx;
+            nus++;
+        }
+        return retval;
+    }
+
+    std::vector<double> phenotype(const std::vector<double> &x, const std::vector<unsigned> &genotype)
+    {
+        // Size will be the vars+constant values (x) and then the number of triplets F u0 u1
+        auto n_triplets = genotype.size() / 3;
+        std::vector<double> retval(m_nvar + m_ncon + n_triplets);
+        // The u0, u1, ... are the values of variables and constants
+        std::copy(x.begin(), x.end(), retval.begin());
+        // We loop and for each triplet compute the corresponding function
+        for (decltype(n_triplets) i = 0u; i < n_triplets; ++i) {
+            auto u0val = retval[genotype[3 * i + 1]];
+            auto u1val = retval[genotype[3 * i + 2]];
+            auto fidx = genotype[3 * i];
+            switch (fidx) {
+                case 0:
+                    retval[i + x.size()] = add(u0val, u1val);
+                    break;
+                case 1:
+                    retval[i + x.size()] = sub(u0val, u1val);
+                    break;
+                case 2:
+                    retval[i + x.size()] = mul(u0val, u1val);
+                    break;
+                case 3:
+                    retval[i + x.size()] = div(u0val, u1val);
+                    break;
+                default:
+                    throw;
+            }
+        }
+        return retval;
+    }
+
+    std::vector<double> mse(const std::vector<unsigned> &genotype, const std::vector<std::vector<double>> &xs,
+                            const std::vector<double> &ys)
+    {
+        assert(m_nvar + m_ncon == xs[0].size());
+        std::vector<double> retval(m_nvar + m_ncon + genotype.size() / 3, 0u);
+        for (decltype(xs.size()) i = 0u; i < xs.size(); ++i) {
+            // compute all values in the phenotype (u0, u1, u2, .... un) at xs[i]
+            auto squared_err = phenotype(xs[i], genotype);
+            // subtract ys[i] and square
+            for (auto &element : squared_err) {
+                element -= ys[i];
+                element *= element;
+            }
+            // Add to retval
+            std::transform(retval.begin(), retval.end(), squared_err.begin(), retval.begin(), std::plus<double>());
+        }
+        return retval;
+    }
+
+    unsigned m_nvar;
+    unsigned m_ncon;
+    unsigned m_nker;
+    std::vector<unsigned> m_kernels;
+    std::mt19937 m_rng;
+};
+
+inline double koza_quintic(const std::vector<double> &x)
+{
+    return std::pow(x[0], 5) - 2 * std::pow(x[0], 3) + x[0];
+}
+
+void generate_data(std::vector<std::vector<double>> &xs, std::vector<double> &ys, unsigned N, double lb, double ub)
+{
+    xs.resize(N);
+    ys.resize(N);
+    for (auto i = 0.; i < N; ++i) {
+        xs[i] = {lb + i / (N - 1) * (ub - lb)};
+        ys[i] = koza_quintic(xs[i]);
+    }
+}
+
+int main()
+{
+    std::random_device rd;  // only used once to initialise (seed) engine
+    std::mt19937 rng(rd()); // random-number engine used (Mersenne-Twister in this case)
+    // One variable, no constants +,-,*,/
+    expression ex(1, 0, {0, 1, 2, 3});
+    // A random genotype
+    auto genotype = ex.random_genotype(5);
+    fmt::print("[{}]\n", fmt::join(genotype, ", "));
+    // And the phenotype evaluated at x = 1.
+    auto phenotype = ex.phenotype({1.}, genotype);
+    fmt::print("{}\n", fmt::join(phenotype, ", "));
+    // Generate Koza data
+    std::vector<std::vector<double>> xs;
+    std::vector<double> ys;
+    generate_data(xs, ys, 10, -3, 3);
+    fmt::print("{}\n", fmt::join(xs, ", "));
+    fmt::print("{}\n", fmt::join(ys, ", "));
+    auto err = ex.mse(genotype, xs, ys);
+    fmt::print("{}\n", fmt::join(err, ", "));
+
+    return 0;
+}
