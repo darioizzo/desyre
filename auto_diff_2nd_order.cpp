a74d5c73468d222b01318c891b2ef09650c62377
Dario Izzo <dario.izzo@gmail.com>
2022-06-17 16:33:42 +0200

    efficient autodiff implemented (2nd order)

--- auto_diff_2nd_order.cpp
+++ auto_diff_2nd_order.cpp
@@ -6,27 +6,6 @@
 
 #include <fmt/ranges.h>
 
-// Binary kernels
-double add(double a, double b)
-{
-    return a + b;
-}
-
-double sub(double a, double b)
-{
-    return a - b;
-}
-
-double mul(double a, double b)
-{
-    return a * b;
-}
-
-double div(double a, double b)
-{
-    return a / b;
-}
-
 struct expression {
     expression(unsigned nvar, unsigned ncon, std::vector<unsigned> kernels,
                decltype(std::random_device{}()) seed = std::random_device{}())
@@ -70,7 +49,7 @@ struct expression {
         assert(n_terminals == x.size());
         // Size will be the vars+constant values (x) and then the number of triplets F u0 u1
         auto n_triplets = genotype.size() / 3;
-        std::vector<double> phenotype(m_nvar + m_ncon + n_triplets);
+        std::vector<double> phenotype(n_terminals + n_triplets);
         // The u0, u1, ... are the values of variables and constants
         std::copy(x.begin(), x.end(), phenotype.begin());
         // We loop and for each triplet compute the corresponding function
@@ -80,16 +59,19 @@ struct expression {
             auto fidx = genotype[3 * i];
             switch (m_kernels[fidx]) {
                 case 0:
-                    phenotype[i + n_terminals] = add(u0, u1);
+                    phenotype[i + n_terminals] = u0 + u1;
                     break;
                 case 1:
-                    phenotype[i + n_terminals] = sub(u0, u1);
+                    phenotype[i + n_terminals] = u0 - u1;
                     break;
                 case 2:
-                    phenotype[i + n_terminals] = mul(u0, u1);
+                    phenotype[i + n_terminals] = u0 * u1;
                     break;
                 case 3:
-                    phenotype[i + n_terminals] = div(u0, u1);
+                    phenotype[i + n_terminals] = u0 / u1;
+                    break;
+                case 4:
+                    phenotype[i + n_terminals] = std::sin(u0);
                     break;
                 default:
                     throw;
@@ -99,7 +81,7 @@ struct expression {
     }
 
     // First order derivatives
-    std::vector<double> dphenotype(const std::vector<unsigned> &genotype, const std::vector<unsigned> &phenotype,
+    std::vector<double> dphenotype(const std::vector<unsigned> &genotype, const std::vector<double> &phenotype,
                                    unsigned idx)
     {
         // Number of terminals (vars and cons)
@@ -122,22 +104,81 @@ struct expression {
             auto fidx = genotype[3 * i];
             switch (m_kernels[fidx]) {
                 case 0:
-                    dphenotype[i + n_terminals] = add(d_u0, d_u1);
+                    dphenotype[i + n_terminals] = d_u0 + d_u1;
                     break;
                 case 1:
-                    dphenotype[i + n_terminals] = sub(d_u0, d_u1);
+                    dphenotype[i + n_terminals] = d_u0 - d_u1;
                     break;
                 case 2:
-                    dphenotype[i + n_terminals] = add(mul(u0, d_u1), mul(d_u0, u1));
+                    dphenotype[i + n_terminals] = u0 * d_u1 + d_u0 * u1;
                     break;
                 case 3:
-                    dphenotype[i + n_terminals] = div(sub(mul(d_u0, u1), mul(u0, d_u1)), mul(u1,u1));
+                    dphenotype[i + n_terminals] = (d_u0 * u1 - d_u1 * u0) / (u1 * u1);
+                    break;
+                case 4:
+                    dphenotype[i + n_terminals] = std::cos(u0) * d_u0;
                     break;
                 default:
                     throw;
             }
         }
         return dphenotype;
+    }
+
+    // Second order derivative
+    std::vector<double> ddphenotype(const std::vector<unsigned> &genotype, const std::vector<double> &phenotype,
+                                    const std::vector<double> &d0phenotype, const std::vector<double> &d1phenotype)
+    {
+        assert(dphenotype.size() == phenotype.size());
+        // Number of terminals (vars and cons)
+        unsigned n_terminals = m_nvar + m_ncon;
+        // Number of triplets (F idx0, idx1 in the chromosome)
+        auto n_triplets = genotype.size() / 3;
+        // Size of the return value will be the same as phenotype
+        std::vector<double> ddphenotype(phenotype.size(), 0.);
+        // We loop and for each triplet compute the corresponding function
+        for (decltype(n_triplets) i = 0u; i < n_triplets; ++i) {
+            // Retrieve the values
+            auto u0 = phenotype[genotype[3 * i + 1]];
+            auto u1 = phenotype[genotype[3 * i + 2]];
+            // Retrieve the derivatives
+            auto d0_u0 = d0phenotype[genotype[3 * i + 1]];
+            auto d0_u1 = d0phenotype[genotype[3 * i + 2]];
+            auto d1_u0 = d1phenotype[genotype[3 * i + 1]];
+            auto d1_u1 = d1phenotype[genotype[3 * i + 2]];
+            // Retrieve the second derivatives
+            auto dd_u0 = ddphenotype[genotype[3 * i + 1]];
+            auto dd_u1 = ddphenotype[genotype[3 * i + 2]];
+            // Retrieve the function
+            auto fidx = genotype[3 * i];
+            switch (m_kernels[fidx]) {
+                // +
+                case 0:
+                    ddphenotype[i + n_terminals] = dd_u0 + dd_u1;
+                    break;
+                // -
+                case 1:
+                    ddphenotype[i + n_terminals] = dd_u0 - dd_u1;
+                    break;
+                // *
+                case 2:
+                    ddphenotype[i + n_terminals] = dd_u0 * u1 + dd_u1 * u0 + d0_u0 * d1_u1 + d1_u0 * d0_u1;
+                    break;
+                // /
+                case 3:
+                    ddphenotype[i + n_terminals] = ((dd_u0 * u1 + d0_u0 * d1_u1 - d0_u1 * d1_u0 - dd_u1 * u0) * u1 * u1
+                                                    - 2 * u1 * d1_u1 * (d0_u0 * u1 - d0_u1 * u0))
+                                                   / u1 / u1 / u1 / u1;
+                    break;
+                // sin
+                case 4:
+                    ddphenotype[i + n_terminals] = -std::sin(u0) * d0_u0 * d1_u0 + std::cos(u0) * dd_u0;
+                    break;
+                default:
+                    throw;
+            }
+        }
+        return ddphenotype;
     }
 
     std::vector<double> mse(const std::vector<unsigned> &genotype, const std::vector<std::vector<double>> &xs,
@@ -212,67 +253,43 @@ void generate_data(std::vector<std::vector<double>> &xs, std::vector<double> &ys
     }
 }
 
-// Usage ./koza n_trials verbosity
+using namespace fmt;
+// Usage ./main n_trials verbosity
 int main(int argc, char *argv[])
 {
-    auto n_trials = std::atoi(argv[1]);
-    auto verbosity = std::atoi(argv[2]);
+    // auto n_trials = std::atoi(argv[1]);
+    // auto verbosity = std::atoi(argv[2]);
 
     std::random_device rd;  // only used once to initialise (seed) engine
     std::mt19937 rng(rd()); // random-number engine used (Mersenne-Twister in this case)
-    // One variable, no constants +,-,*,/
-    expression ex(1, 0, {0, 1, 2, 3});
-    // A random genotype
-    auto genotype = ex.random_genotype(5);
-    fmt::print("[{}]\n", fmt::join(genotype, ", "));
-    // And the phenotype evaluated at x = 1.
-    auto phenotype = ex.phenotype(genotype, {1.});
-    fmt::print("{}\n", fmt::join(phenotype, ", "));
-    // Generate Koza data
-    std::vector<std::vector<double>> xs;
-    std::vector<double> ys;
-    generate_data(xs, ys, 10, -3, 3);
-    fmt::print("{}\n", fmt::join(xs, ", "));
-    fmt::print("{}\n", fmt::join(ys, ", "));
-    // Print the error
-    auto err = ex.mse(genotype, xs, ys);
-    fmt::print("{}\n", fmt::join(err, ", "));
-    // Print the fitness
-    auto fit = ex.fitness(genotype, xs, ys);
-    fmt::print("{}\n", fmt::join(fit, ", "));
-    // Mutate (1,2,3 and check)
-    fmt::print("[{}]\n", fmt::join(ex.mutation(genotype, 1), ", "));
-    fmt::print("[{}]\n", fmt::join(ex.mutation(genotype, 2), ", "));
-    fmt::print("[{}]\n", fmt::join(ex.mutation(genotype, 3), ", "));
-    // Run the evolution
-    // We run n_trials experiments
-    auto ERT = 0u;
-    for (auto j = 0u; j < n_trials; ++j) {
-        // We let each run to convergence
-        auto best_x = ex.random_genotype(20);
-        auto best_f = ex.fitness(best_x, xs, ys);
-        auto count = 0u;
-        count++;
-        while (true) {
-            for (auto i = 0u; i < 4u; ++i) {
-                auto new_x = ex.mutation(best_x, 5);
-                auto new_f = ex.fitness(new_x, xs, ys);
-                count++;
-                if (new_f[0] <= best_f[0]) {
-                    best_x = new_x;
-                    best_f = new_f;
-                    // Only if verbosity is > 0
-                    if (verbosity > 0) {
-                        fmt::print("New Best is {} at {} fevals)\n", best_f, count);
-                    }
-                }
-            }
-            if (best_f[0] < 1e-10) {
-                break;
-            }
-        }
-        ERT += count;
-    }
-    fmt::print("ERT is {}\n", ERT / n_trials);
+    fmt::print("Working with f=[x, c, xc, sin(x), x+sin(x), (x+sin(cx))/x] \n");
+    // One variable, one constants +,-,*,/, sin
+    expression ex(1, 1, {0, 1, 2, 3, 4});
+    // Manually assemble (x + sin(cx)) / x
+    std::vector<unsigned> genotype = {2, 0, 1, 4, 2, 0, 0, 0, 3, 3, 4, 0};
+    // Compute the phenotype at x=1, c=0.5
+    auto f = ex.phenotype(genotype, {1., 1. / 2.});
+    print("f: {}\n", f);
+    // Compute the dphenotype at x=1, c=0.5
+    // d dx
+    auto f_dx = ex.dphenotype(genotype, f, 0);
+    print("dfdx: {}\n", f_dx);
+    // d dc
+    auto f_dc = ex.dphenotype(genotype, f, 1);
+    print("dfdc: {}\n", f_dc);
+    // Compute the ddphenotype at x=1, c=0.5
+    // d dxdx
+    auto f_dx_dx = ex.ddphenotype(genotype, f, f_dx, f_dx);
+    print("dfdxdx: {}\n", f_dx_dx);
+    // d dcdc
+    auto f_dc_dc = ex.ddphenotype(genotype, f, f_dc, f_dc);
+    print("dfdcdc: {}\n", f_dc_dc);
+    // d dxdc
+    auto f_dx_dc = ex.ddphenotype(genotype, f, f_dx, f_dc);
+    print("dfdxdc: {}\n", f_dx_dc);
+    // d dcdx
+    auto f_dc_dx = ex.ddphenotype(genotype, f, f_dc, f_dx);
+    print("dfdcdx: {}\n", f_dc_dx);
+
     return 0;
 }
